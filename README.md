796984dbed2f042f1d65e7b8a131667268eee555
german.oshuev <german.oshuev@sredaslotions.com>
2019-11-02 22:31:10 +0300

    add payload and update README.md
    install how to

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rpull
-python shell trick for recursive git pull 
+python3 shell trick for recursive git pull
+
+### what is it
+if you type "rpull", you can recursive update all rep's in current path  
+
+#### easy install trick
+
+```bash
+mkdir -p ~/.bash_tricks && curl https://raw.githubusercontent.com/germanwb/rpull/master/rpull.py --output ~/.bash_tricks/rpull.py && echo 'alias rpull='python3 ~/.bash_tricks/rpull.py --path \$\(pwd\)'' >> ~/.bash_aliases && alias rpull='python3 ~/.bash_tricks/rpull.py --path $(pwd)
+```
---
+++ rpull.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+
+def check_it(root_path: str):
+    list_of_dir = os.listdir(root_path)
+    checked_dirs = []
+    for p in list_of_dir:
+        full_path = os.path.join(root_path, p)
+        if not os.path.isdir(full_path):
+            continue
+        elif not os.path.isdir(os.path.join(full_path, '.git')):
+            continue
+        else:
+            checked_dirs.append(p)
+    return checked_dirs
+
+
+def recursive_pull(list_of_repo: list, root_dir: str):
+    for repo in list_of_repo:
+        print('%+50s' % f'\n\n\n=====_update {repo}_=====')
+        os.system(f'cd {os.path.join(root_dir, repo)}&& git checkout master && git pull')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='pls set path to root of recurse')
+    parser.add_argument('--path', type=str, help='root path')
+    args = parser.parse_args()
+    path = args.path
+    if not os.path.isdir(path) or path is None:
+        print('error path')
+        exit(-1)
+    gits = check_it(root_path=path)
+    print(f'start recursive pull in {path} ')
+    if len(gits) == 0:
+        print("no git dir's")
+    else:
+        print(f'list of repos {gits}, press ENTER to start (or ctl+C for canceling)')
+        input()
+    recursive_pull(list_of_repo=gits, root_dir=path)
+    print('Done')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Canceling")
+        exit(0)
