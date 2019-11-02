7fab90348ad531ac9c46d16a27ba768b26fafd7f
german.oshuev <german.oshuev@sredaslotions.com>
2019-11-02 23:05:27 +0300

    add disabled switch to master

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 python3 shell trick for recursive git pull
 
 ### what is it
-if you type "rpull", you can recursive update all rep's in current path  
+if you type "rpull", you can recursive pull all rep's in current path  , or m_rpull for switch to master and pull repos in curr path  
 
 #### easy install trick
 
 ```bash
-mkdir -p ~/.bash_tricks && curl https://raw.githubusercontent.com/germanwb/rpull/master/rpull.py --output ~/.bash_tricks/rpull.py && echo 'alias rpull='python3 ~/.bash_tricks/rpull.py --path \$\(pwd\)'' >> ~/.bash_aliases && alias rpull='python3 ~/.bash_tricks/rpull.py --path $(pwd)
+mkdir -p ~/.bash_tricks && curl https://raw.githubusercontent.com/germanwb/rpull/master/rpull.py --output ~/.bash_tricks/rpull.py; echo "alias rpull='python3 ~/.bash_tricks/rpull.py --path \$\(pwd\)'" >> ~/.bash_aliases; alias rpull='python3 ~/.bash_tricks/rpull.py --path $(pwd)'; echo "alias m_rpull='python3 ~/.bash_tricks/rpull.py --path \$\(pwd\) -switch'" >> ~/.bash_aliases; alias m_rpull='python3 ~/.bash_tricks/rpull.py --path $(pwd) -switch'
 ```
--- rpull.py
+++ rpull.py
@@ -16,15 +16,20 @@ def check_it(root_path: str):
     return checked_dirs
 
 
-def recursive_pull(list_of_repo: list, root_dir: str):
+def recursive_pull(list_of_repo: list, root_dir: str, switch: bool):
     for repo in list_of_repo:
         print('%+50s' % f'\n\n\n=====_update {repo}_=====')
-        os.system(f'cd {os.path.join(root_dir, repo)}&& git checkout master && git pull')
+        if switch:
+            bash = f'cd {os.path.join(root_dir, repo)}&& git checkout master && git pull'
+        else:
+            bash = f'cd {os.path.join(root_dir, repo)}&& git pull'
+        os.system(bash)
 
 
 def main():
     parser = argparse.ArgumentParser(description='pls set path to root of recurse')
     parser.add_argument('--path', type=str, help='root path')
+    parser.add_argument('-switch', action='store_true')
     args = parser.parse_args()
     path = args.path
     if not os.path.isdir(path) or path is None:
@@ -32,12 +37,15 @@ def main():
         exit(-1)
     gits = check_it(root_path=path)
     print(f'start recursive pull in {path} ')
+    master_switch = args.switch
+    if master_switch:
+        print('and switch to master')
     if len(gits) == 0:
         print("no git dir's")
     else:
         print(f'list of repos {gits}, press ENTER to start (or ctl+C for canceling)')
         input()
-    recursive_pull(list_of_repo=gits, root_dir=path)
+    recursive_pull(list_of_repo=gits, root_dir=path, switch=master_switch)
     print('Done')
 
 
